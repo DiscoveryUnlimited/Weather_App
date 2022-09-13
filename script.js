315fcad6168c979beedb46f1a655a9690a7787b4
DiscoveryUnlimited <jason.d.pagan89@gmail.com>
2022-09-13 16:02:14 -0400

    Update HTML CSS and JS

--- script.js
+++ script.js
@@ -3,40 +3,83 @@ import { getCurrentWeather } from "./weather-helper-functions.js";
 
 import { findWeatherImg } from "./image-helper-functions.js";
 
-// Create a card using input from location field
-document.getElementById("input_container").addEventListener("submit", (evt) => {
-  evt.preventDefault();
 
-  //document.getElementById("input_container").reset();
+const defaultImg = "https://images.unsplash.com/photo-1516912481808-3406841bd33c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&q=80";
+
+
+// Create a card using input from location field
+document.getElementById("input_container").addEventListener("submit", async(evt) => {
+  evt.preventDefault();
 
   //location. temperature, weather condition, weather condition img.
   //grab user inputs(location) from the input_container, and store it in variables.
-  const locationInput = document.getElementById("input_container").value; //TODO change the ID.
+  const locationInput = document.getElementById("user_input").value; //TODO change the ID.
+
+  // Weather call
+  const weather = await getCurrentWeather(locationInput);
 
   //to create a card under id"card_container" to contain the display section.
   const card = document.createElement("div"); //card_container=>card & img; card=>(location, tem)
   card.setAttribute("class", "card");
-  card_container.appendChild(card);
+  card.setAttribute("style", "background-color: #faf1f1;");
+  card.style.height = "fit-content";
+  card.style.margin = "15px;";
+  
+
+  const cardBody = document.createElement("div");
+  cardBody.setAttribute("class", "card-body");
 
   //create location display
   const locationDisplay = document.createElement("h5");
   locationDisplay.setAttribute("class", "card-title");
-  // locationDisplay.innerText=  TODO
-  card.appendChild(locationDisplay);
+  locationDisplay.innerText = locationInput;
+  cardBody.appendChild(locationDisplay);
 
+
+  //Temperature Display
+  const temperature = weather.data[0].temp;
   const tempDisplay = document.createElement("h6");
   tempDisplay.setAttribute("class", "card-subtitle mb-2 text-muted");
-  // tempDisplay.innerText=  TODO
-  card.appendChild(tempDisplay);
+  // TODO
+  tempDisplay.innerText = temperature;
+  cardBody.appendChild(tempDisplay);
 
+
+  //Weather Condition
+  const condition = weather.data[0].weather.description;
   const weaConDisplay = document.createElement("p");
   weaConDisplay.setAttribute("class", "card-text");
-  // weaConDisplay.innerText=  TODO
-  card.appendChild(weaConDisplay);
+  // TODO
+  weaConDisplay.innerText = condition;
+  cardBody.appendChild(weaConDisplay);
 
   const imgDisplay = document.createElement("img");
   imgDisplay.setAttribute("class", "card-img-top");
   imgDisplay.setAttribute("id", "image");
-  card_container.appendChild(imgDisplay);
-  // findWeatherImg(); //TODO
+  imgDisplay.setAttribute("src", defaultImg);
+  cardBody.appendChild(imgDisplay);
+
+  //searchable condition and update image
+  var words = condition.split(" ");
+  console.log(words.length);
+  if (words.length > 1){
+  var search = words[0];
+  console.log(search);
+    for (var i = 1; i <= words.length - 1; i++) {
+      console.log(i);
+      let word = words[i];
+
+      let wordPlus ="+" + word;
+      search += wordPlus;
+    }
+    
+    findWeatherImg(search);
+  }
+
+  card.appendChild(cardBody);
+
+  card_container.appendChild(card);
+  
+  document.getElementById("search_bar").reset();
+
 });
