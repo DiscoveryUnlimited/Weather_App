eba1a51b54b4108673f08ff80ce5f7e17bd81013
aaronw9989 <williamsa493@strose.edu>
2022-09-13 10:16:45 -0500

    Cleaned up weather functions code.

--- weather-helper-functions.js
+++ weather-helper-functions.js
@@ -1,28 +1,4 @@
-// Weather API Notes: 
-// Source: https://www.weatherbit.io/
 
-//https://api.weatherbit.io/v2.0/current?
-// lat=35.7796
-// &lon=-78.6382
-// &key=API_KEY
-
-// -- API Key --
-// 0632122ec59f467cb1b46c3567c0b3b0
-
-// https://api.weatherbit.io/v2.0/current?lat=38.8894&lon=-77.0352&key=0632122ec59f467cb1b46c3567c0b3b0
-
-
-// https://api.weatherbit.io/v2.0/current?&city=washington,DC&key=0632122ec59f467cb1b46c3567c0b3b0
-// &city=Raleigh,NC
-// &city=washington,DC
-// Other options 
-// &city=Raleigh&country=US
-// &city=Raleigh,NC
-// &city=Raleigh,North+Carolina
-
-// Using postal code
-// &postal_code=20001
-// https://api.weatherbit.io/v2.0/current?&postal_code=20001&key=0632122ec59f467cb1b46c3567c0b3b0
 
 
 
@@ -58,8 +34,6 @@ async function getCurrentWeatherZip(postalCode) {
 
     const API_URL = `https://api.weatherbit.io/v2.0/current?&postal_code=${postalCode}&key=0632122ec59f467cb1b46c3567c0b3b0`;
 
-    // const API_URL = "https://api.kanye.rest";
-
     const response = await fetch(API_URL);
 
     const weather = await response.json(); 
@@ -73,8 +47,6 @@ async function getCurrentWeatherZip(postalCode) {
 async function getCurrentWeatherCityName(cityName) {
 
     const API_URL = `https://api.weatherbit.io/v2.0/current?&city=${cityName}&key=0632122ec59f467cb1b46c3567c0b3b0`;
-
-    // const API_URL = "https://api.kanye.rest";
 
     const response = await fetch(API_URL);
 
