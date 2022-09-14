3a0890c02395dd1c9ce740bd2c8f316a26954478
liangyaliao <liaoliangyan@gmail.com>
2022-09-13 21:07:34 -0400

    update input's temperature and weather condition

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ import { findWeatherImg } from "./image-helper-functions.js";
 const defaultImg =
   "https://images.unsplash.com/photo-1516912481808-3406841bd33c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&q=80";
 
-const defaultZipCode = 27519;
-const locationInput = defaultZipCode;
+//const defaultZipCode = 27519;
+const locationInput = 27519;
 // Weather call
-let weather = await getCurrentWeather(locationInput); //TODO
+let weather = await getCurrentWeather(locationInput);
 
 //to create a card under id"card_container" to contain the display section.
 const card = document.createElement("div"); //card_container=>card & img; card=>(location, tem)
@@ -27,6 +27,7 @@ locationDisplay.setAttribute("class", "card-title");
 locationDisplay.setAttribute("id", "card-title");
 locationDisplay.innerText = locationInput;
 cardBody.appendChild(locationDisplay);
+card.appendChild(cardBody);
 
 //Temperature Display
 const temperature = weather.data[0].temp;
@@ -35,7 +36,7 @@ tempDisplay.setAttribute("class", "card-subtitle mb-2 text-muted");
 // TODO
 tempDisplay.innerText = temperature;
 cardBody.appendChild(tempDisplay);
-
+console.log(temperature);
 //Weather Condition
 const condition = weather.data[0].weather.description;
 const weaConDisplay = document.createElement("p");
@@ -52,12 +53,12 @@ cardBody.appendChild(imgDisplay);
 
 //searchable condition and update image
 var words = condition.split(" ");
-console.log(words.length);
+//console.log(words.length);
 if (words.length > 1) {
   var search = words[0];
-  console.log(search);
+  //console.log(search);
   for (var i = 1; i <= words.length - 1; i++) {
-    console.log(i);
+    //console.log(i);
     let word = words[i];
 
     let wordPlus = "+" + word;
@@ -66,8 +67,6 @@ if (words.length > 1) {
 
   findWeatherImg(search);
 }
-
-card.appendChild(cardBody);
 
 document.getElementById("card_container").appendChild(card);
 
@@ -91,20 +90,21 @@ document
     //Temperature Display
     let temperature = weather.data[0].temp;
     document.getElementsByClassName("card-subtitle").innerText = temperature;
-
+    //console.log(temperature);
+    tempDisplay.innerText = temperature;
     //Weather Condition
     let condition = weather.data[0].weather.description;
     //weaConDisplay.innerText = condition;
     document.getElementsByClassName("card-text").innerText = condition;
-
+    weaConDisplay.innerText = condition;
     //searchable condition and update image
     var words = condition.split(" ");
     console.log(words.length);
     if (words.length > 1) {
       var search = words[0];
-      console.log(search);
+      //console.log(search);
       for (var i = 1; i <= words.length - 1; i++) {
-        console.log(i);
+        //console.log(i);
         let word = words[i];
 
         let wordPlus = "+" + word;
