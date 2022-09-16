b6710966ab900f1024d59065dc5d1a1a337f308a
pchandt <pcthakuri26@gmail.com>
2022-09-16 07:29:54 -0700

    final changes

--- newsfeed-helper-functions.js
+++ newsfeed-helper-functions.js
@@ -5,11 +5,12 @@ async function getCurrentNews(locationInput) {
     const API_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${locationInput}&api-key=qllmMtHLGugS70k6UGMjIpaNHs3zeWos`;
 
     const response = await fetch(API_URL);
-    console.log(response);
     const searchNews = await response.json();
-    console.log(searchNews);
-    newsArticles = searchNews.response.docs[0].abstract;
-    console.log(newsArticles);
+    const result = searchNews.response.docs;
+
+    var rand = Math.floor(Math.random() * result.length);
+    newsArticles = result[rand].abstract;
+
     return newsArticles;
   }
 }
